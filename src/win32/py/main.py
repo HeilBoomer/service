1f73eb1f885b53d29d9559fd36e45dd31f133e69
HeilBoomer <0606062006ak@gmail.com>
2020-12-10 23:17:12 +0300

    Started Windows service

--- src/win32/py/main.py
+++ src/win32/py/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from SMWinservice import SMWinservice
-
+import socket
+port = 42069
+ip = "127.0.0.1"
 
 class SahitoService(SMWinservice):
     _svc_name_ = 'sahito'
@@ -14,8 +16,16 @@ class SahitoService(SMWinservice):
         self.is_running = False
 
     def main(self):
-        pass
+        s = socket.socket()
 
+        s.bind((ip,port))
+        s.listen(1)
+        while self.is_running:
+            c,addr = s.accept()
+            data = c.recv(1024).decode()
+            if not data:
+                continue
+            print(" \x1b[1;32m=> \x1b[1;37mReceived message: \x1b[;m", str(data))
 
 if __name__ == '__main__':
     SahitoService.parse_command_line()
